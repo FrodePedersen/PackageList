e8b5a79513d39689f4a955d8ae10a4a26bf1e0ee
Frode S. N. Pedersen <fsnpedersen@gmail.com>
2017-08-20 13:30:38 +0200

    initialized PackageList

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PackageList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
